caf36bed01150a0cc6506e22e17ffb044bc279eb
Stanislau <stanislav.pinsk@gmail.com>
2023-06-23 14:08:18 +0200

    Modificate file

--- Task_18.py
+++ Task_18.py
@@ -5,17 +5,34 @@
 # . Последняя строка
 # содержит число X
 
+# Решение, если числа в массиве идут от 1 до n по порядку
+# x = int(input())
+# array = []
+# for i in range(1, x+1):
+#     array.append(i)
+#     print(array[i-1], end = ' ')
+# print()
+# y = int(input())
+# if y >= array[x-1]:
+#     print(array[x-1])
+# elif y <= array[0]:
+#     print(array[0])
+# else:
+#     print(y)
 
+# Решение, если числа в массиве стоят рандомные числа от 1 до 100 (можно изменить) или рандном заменить на инпут для ввода с клавиатуры
+from random import randint
 x = int(input())
 array = []
 for i in range(1, x+1):
-    array.append(i)
+    array.append(randint(1,100))
     print(array[i-1], end = ' ')
 print()
 y = int(input())
-if y >= array[x-1]:
-    print(array[x-1])
-elif y <= array[0]:
-    print(array[0])
-else:
-    print(y)
+deff = abs(array[0] - y)
+deff_values = array[0]
+for i in array:
+    if deff > abs(i - y):
+        deff = abs(i - y)
+        deff_values = i
+print(deff_values)
