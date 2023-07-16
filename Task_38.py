faea10242f80f34c1e78f783ab355a132812df2d
Stanisław Wasiliczin <128746979+Smilov@users.noreply.github.com>
2023-07-16 12:01:39 +0200

    Update Task_38.py

--- Task_38.py
+++ Task_38.py
@@ -7,7 +7,9 @@ def show_menu():
     print("2. Найти абонента по фамилии")
     print("3. Найти абонента по номеру телефона")
     print("4. Добавить абонента в справочник")
-    print("5. Закончить работу")
+    print("5. Изменить данные абонента")
+    print("6. Удалить абонента из справочника")
+    print("7. Закончить работу")
     choice = int(input())
     return choice
 
@@ -40,6 +42,37 @@ def add_entry(phonebook):
     phonebook.append(entry)
     print("Абонент успешно добавлен.")
 
+def update_entry(phonebook, name):
+    found_entries = [index for index, entry in enumerate(phonebook) if entry[0].lower() == name.lower()]
+    if found_entries:
+        print(f"Найдено {len(found_entries)} записей с именем {name}.")
+        for index in found_entries:
+            entry = phonebook[index]
+            print("Текущая информация:")
+            print('|'.join(entry))
+            print("Введите новые данные:")
+            last_name = input("Фамилия: ")
+            first_name = input("Имя: ")
+            phone_number = input("Номер телефона: ")
+            description = input("Примечание: ")
+            phonebook[index] = [last_name, first_name, phone_number, description]
+            print("Данные успешно обновлены.")
+    else:
+        print("Абонент не найден.")
+
+def delete_entry(phonebook, name):
+    found_entries = [entry for entry in phonebook if entry[0].lower() == name.lower()]
+    if found_entries:
+        print(f"Найдено {len(found_entries)} записей с именем {name}.")
+        confirm = input("Вы уверены, что хотите удалить эти записи? (y/n): ")
+        if confirm.lower() == 'y':
+            phonebook[:] = [entry for entry in phonebook if entry[0].lower() != name.lower()]
+            print("Записи успешно удалены.")
+        else:
+            print("Удаление отменено.")
+    else:
+        print("Абонент не найден.")
+
 def work_with_phonebook():
     choice = show_menu()
 
@@ -53,7 +86,7 @@ def work_with_phonebook():
         reader = csv.reader(file)
         phonebook = list(reader)
 
-    while choice != 5:
+    while choice != 7:
         if choice == 1:
             print_phonebook(phonebook)
         elif choice == 2:
@@ -67,7 +100,19 @@ def work_with_phonebook():
             with open('phonebook.csv', 'a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(phonebook[-1])
-        elif choice > 5:
+        elif choice == 5:
+            name = input("Введите фамилию для обновления данных: ")
+            update_entry(phonebook, name)
+            with open('phonebook.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(phonebook)
+        elif choice == 6:
+            name = input("Введите фамилию для удаления: ")
+            delete_entry(phonebook, name)
+            with open('phonebook.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(phonebook)
+        elif choice > 7:
             print("Вы ввели слишком большое значение, попробуйте еще раз.")
         print()
         choice = show_menu()
